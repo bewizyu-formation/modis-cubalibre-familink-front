f85c6ea41c0531a651f09ce6490ffe5ae507e281
KurayamiNoDaiTenshi <angelo67170@gmail.com>
2017-10-13 12:42:27 +0200

    refactoring project structure (#3)
    * refactoring project structure
    
    * change basePath

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {RouterModule} from "@angular/router";
-import {ROUTES} from "./app.routes";
+import {HomeComponent} from './component/home/home.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
 
-import { SignupComponent } from './signup/signup.component';
+import {SignupComponent} from './component/signup/signup.component';
 
 
 @NgModule({
---
+++ src/app/app.routes.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var home_component_1 = require("./home/home.component");
+exports.PATH_HOME = 'home';
+exports.ROUTES = [
+    { path: exports.PATH_HOME, component: home_component_1.HomeComponent },
+];
