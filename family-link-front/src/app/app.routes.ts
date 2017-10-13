3cc4ad72d9822e34a9a6a8836e3537b2007cf595
Angelo Basso <angelo67170@gmail.com>
2017-10-13 12:21:53 +0200

    refactoring project structure

--- family-link-front/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-export const PATH_HOME:string = 'home';
-export const ROUTES: Routes = [
-{ path: PATH_HOME, component: HomeComponent },
-];
