c64c38dbeab8d496be54403ee2178b0330bdc19b
avlas <avlas@gmail.com>
2017-10-16 18:10:44 +0200

    refactor modules & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,56 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ROUTES } from './app.routes';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatFormFieldModule } from '@angular/material';
-import { MatInputModule } from '@angular/material';
-import { MatButtonModule } from '@angular/material';
-import { MatListModule } from '@angular/material';
-import { MatCardModule } from '@angular/material';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { ChooseGroupComponent } from './components/groups/choose-group/choose-group.component';
-import { CreateGroupComponent } from './components/groups/create-group/create-group.component';
-import { ListGroupComponent } from './components/groups/list-group/list-group.component';
-import { GroupComponent } from './components/groups/group/group.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule, MatMenuModule, MatToolbarModule, MatIconRegistry} from '@angular/material';
+import {HeaderComponent} from './components/header/header.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule, MatInputModule, MatButtonModule, MatListModule, MatCardModule} from '@angular/material';
+import {SignupComponent} from './components/signup/signup.component';
+import {ChooseGroupComponent} from './components/groups/choose-group/choose-group.component';
+import {CreateGroupComponent} from './components/groups/create-group/create-group.component';
+import {ListGroupComponent} from './components/groups/list-group/list-group.component';
+import {GroupComponent} from './components/groups/group/group.component';
 
 
 @NgModule({
   declarations: [
-      AppComponent,
-      HomeComponent,
-      SignupComponent,
-      ChooseGroupComponent,
-      CreateGroupComponent,
-      ListGroupComponent,
-      GroupComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    ChooseGroupComponent,
+    CreateGroupComponent,
+    ListGroupComponent,
+    GroupComponent,
+    HeaderComponent,
   ],
   imports: [
-      RouterModule.forRoot(ROUTES),
-      BrowserModule,
-      BrowserAnimationsModule,
-      FormsModule,
-      ReactiveFormsModule,
-      MatFormFieldModule,
-      MatInputModule,
-      MatButtonModule,
-      MatListModule,
-      MatCardModule,
+    RouterModule.forRoot(ROUTES),
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(matIconRegistry : MatIconRegistry, domSanitizer : DomSanitizer) {
+    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg'));
+  }
+}
