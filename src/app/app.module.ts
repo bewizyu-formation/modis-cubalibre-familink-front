5599047b7e91544cb66be3dc30798e0061e1ce9b
Angelo Basso <angelo67170@gmail.com>
2017-10-17 11:27:00 +0200

    signin component work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { RouterModule } from '@angular/router';
-import { ROUTES } from './app.routes';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
 
-import { SignupComponent } from './components/signup/signup.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule, MatMenuModule, MatToolbarModule,MatIconRegistry } from '@angular/material';
-import { HeaderComponent } from './components/header/header.component';
-import { HttpClientModule } from '@angular/common/http';
+import {SignupComponent} from './components/signup/signup.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatIconRegistry,
+  MatInputModule,
+  MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
+import {HeaderComponent} from './components/header/header.component';
+import {HttpClientModule} from '@angular/common/http';
+import {SignInComponent} from './components/sign-in/sign-in.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -19,6 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     SignupComponent,
     HeaderComponent,
+    SignInComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,6 +39,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     MatIconModule,
     HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
