ffaa15e5b302e5157f3e260f13685930947a8a1e
KurayamiNoDaiTenshi <angelo67170@gmail.com>
2017-10-17 22:03:37 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,8 @@ import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { ChooseGroupComponent } from './components/groups/choose-group/choose-group.component';
 
-export const PATH_HOME = 'home';
-export const PATH_SIGNIN = 'signin';
-export const PATH_GROUP = 'group';
 export const ROUTES: Routes = [
-  { path: PATH_HOME, component: HomeComponent },
-  { path: PATH_SIGNIN, component: SignInComponent },
-  { path: PATH_GROUP, component: ChooseGroupComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SignInComponent },
+  { path: 'group', component: ChooseGroupComponent },
 ];
